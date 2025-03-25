6ed0a78829bc5ec203431cadb67e98e10d1908f7
Tency Titus <tencytitus1@gmail.com>
2025-03-25 13:20:45 +0530

    Update test data command to include comments and likes

--- blog/management/commands/create_test_data.py
+++ blog/management/commands/create_test_data.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from blog.models import Post
+from blog.models import Post, Comment
 from django.utils import timezone
 import random
 
 class Command(BaseCommand):
-    help = 'Creates test blog posts'
+    help = 'Creates test blog posts, comments, and likes'
 
     def handle(self, *args, **kwargs):
         # Get all users
@@ -39,8 +39,23 @@ class Command(BaseCommand):
             }
         ]
         
+        # Sample comments
+        comments_data = [
+            "Great article! Very informative and well-written.",
+            "Thanks for sharing these insights. I learned a lot.",
+            "This is exactly what I was looking for. Well explained!",
+            "Interesting perspective. Would love to see more content like this.",
+            "Very helpful tutorial. Keep up the great work!",
+            "Clear and concise explanation. Perfect for beginners.",
+            "I've been using these techniques and they work great.",
+            "Looking forward to more articles from you!",
+            "This helped me solve a problem I was stuck on. Thank you!",
+            "Excellent breakdown of complex concepts."
+        ]
+        
         # Create posts
         posts_created = 0
+        all_posts = []
         for post_data in posts_data:
             # Randomly select a user as the author
             author = random.choice(users)
@@ -52,8 +67,35 @@ class Command(BaseCommand):
                 author=author,
                 date_posted=timezone.now()
             )
+            all_posts.append(post)
             posts_created += 1
             
-            self.stdout.write(self.style.SUCCESS(f'Created post: {post.title} by {author.username}'))
+            # Add random likes to the post
+            num_likes = random.randint(1, len(users))
+            liking_users = random.sample(list(users), num_likes)
+            post.likes.set(liking_users)
+            
+            # Add random comments
+            num_comments = random.randint(2, 5)
+            for _ in range(num_comments):
+                commenter = random.choice(users)
+                comment_text = random.choice(comments_data)
+                Comment.objects.create(
+                    post=post,
+                    author=commenter,
+                    content=comment_text,
+                    date_posted=timezone.now()
+                )
+            
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Created post: {post.title} by {author.username} '
+                    f'with {num_comments} comments and {num_likes} likes'
+                )
+            )
         
-        self.stdout.write(self.style.SUCCESS(f'Successfully created {posts_created} test posts')) 
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully created {posts_created} test posts with comments and likes'
+            )
+        ) 
